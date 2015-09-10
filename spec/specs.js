4bdb8ccdd3df82ec38d81639fa4b0b736c1b092d
Alexa Zeazas & Michael Summers <student@epicodus.com>
2015-09-10 10:40:24 -0700

    Write spec for converting double-digit number to roman numeral, pass.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('romanate', function() {
   it("converts a single-digit number to its roman numeral form", function() {
     expect(romanate(5)).to.equal("V");
   });
+
+  it("converts a double-digit number to its roman numeral form", function() {
+    expect(romanate(13)).to.equal("XIII");
+  });
 });
