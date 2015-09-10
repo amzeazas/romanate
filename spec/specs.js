cf745c54828bd96f16f621ac835130070602ff97
Alexa Zeazas & Michael Summers <student@epicodus.com>
2015-09-10 10:42:01 -0700

    Write spec for converting triple-digit number to roman numeral, pass.

--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,8 @@ describe('romanate', function() {
   it("converts a double-digit number to its roman numeral form", function() {
     expect(romanate(13)).to.equal("XIII");
   });
+
+  it("converts a triple-digit number to its roman numeral form", function() {
+    expect(romanate(112)).to.equal("CXII");
+  });
 });
